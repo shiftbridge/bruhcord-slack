10f75bfbd3829ff24ef6c9f67dcc7fddb22183b5
ShiftBridge <121209796+shiftbridge@users.noreply.github.com>
2025-09-13 22:09:03 -0400

    Sentry Fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-import "../instrument.mjs"; // must be first
+import "../instrument.mjs";
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
@@ -8,31 +8,19 @@ import { serve } from "inngest/express";
 import chatRoutes from "./routes/chat.route.js";
 import cors from "cors";
 import * as Sentry from "@sentry/node";
-
 const app = express();
-
-// Sentry request + tracing handlers FIRST
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
-
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware());
-
-app.get("/debug-sentry", () => {
+app.use(clerkMiddleware()); // req.auth will be available in the request object
+app.get("/debug-sentry", (req, res) => {
   throw new Error("My first Sentry error!");
 });
-
 app.get("/", (req, res) => {
   res.send("Hello World! 123");
 });
-
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
-
-// Sentry error handler LAST
-app.use(Sentry.Handlers.errorHandler());
-
+Sentry.setupExpressErrorHandler(app);
 const startServer = async () => {
   try {
     await connectDB();
@@ -43,10 +31,8 @@ const startServer = async () => {
     }
   } catch (error) {
     console.error("Error starting server:", error);
-    process.exit(1);
+    process.exit(1); // Exit the process with a failure code
   }
 };
-
 startServer();
-
 export default app;
